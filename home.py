698d7f3000a2f2d0d4bae1870b3e5119c7264ac6
Yash Soni <106722471+Its-YashSoni@users.noreply.github.com>
2023-11-30 09:52:04 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,15 +1,12 @@
 import streamlit as st
 import numpy as np
-import cv2
 import tensorflow as tf
 from PIL import Image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input
 
 st.set_page_config(
         page_title="AAYU",
         page_icon="logo.jpg",
         layout="centered"
-
 )
 
 def app():
@@ -47,18 +44,19 @@ def app():
                 8:'Sandalwood',
                 9:'Turmeric'
                 }
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype = np.uint8)
-        opencv_image = cv2.imdecode(file_bytes,1)
-        opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-        resized = cv2.resize(opencv_image, (224,224))
+       
 
         img = Image.open(uploaded_file)
         img = img.resize((224, 224))
+
         
+        img_pred = Image.open("test6.jpg")
+        img_pred = img_pred.resize((224,224))
+        img_pred = np.asarray(img_pred)
+
+        img_pred=img_pred/255.
     
-        resized = mobilenet_v2_preprocess_input(resized)
-        img_reshape = resized[np.newaxis,...]    
-        predictions = model.predict(img_reshape)
+        predictions = model.predict(img_pred)
         prediction = predictions.argmax()
         prob = predictions[0][prediction]
         
